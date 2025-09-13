1cda671866ab42d3d89a141ddbbfabd4da32765c
Jonathan Carney <joncar@udel.edu>
2025-09-13 01:16:22 -0400

    fixed page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,7 +8,16 @@ type Props = Omit<ImageProps, "src"> & {
     srcDark: string;
 };
 
+const ThemeImage = (props: Props) => {
+    const { srcLight, srcDark, ...rest } = props;
 
+    return (
+        <>
+            <Image {...rest} src={srcLight} className="imgLight" />
+            <Image {...rest} src={srcDark} className="imgDark" />
+        </>
+    );
+};
 
 export default function Home() {
     return (
